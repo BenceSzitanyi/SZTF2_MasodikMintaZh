9b5b2e253fc88e73026c786a088ebe6dd12ac9c2
BenceSzitanyi <szitka0830@gmail.com>
2024-11-29 09:08:28 +0100

    Add interface

---
+++ MintaZH/IJáték.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH
+{
+    internal interface IJáték
+    {
+        public string Nev { get; }
+        public bool JátszhatVele(int eletkor);
+    }
+}

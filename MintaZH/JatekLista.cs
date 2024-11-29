ee5ab8113a05326e24f33770da04ce32e0ea9184
BenceSzitanyi <szitka0830@gmail.com>
2024-11-29 09:15:10 +0100

    liostaelem

--- MintaZH/JatekLista.cs
+++ MintaZH/JatekLista.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace MintaZH
 {
-    internal class JatekLista<T> where T : IJáték
+    internal class ListaElem<T>
     {
+        public T Tartalom { get; set; }
+        public ListaElem<T> Kovetkezo { get; set; }
+    }
+    internal class JatekLista<T> where T : IJáték
+    {   
         public int EletkorKorlat { get; set; }
         public JatekLista(int eletkorKorlat)
         {
